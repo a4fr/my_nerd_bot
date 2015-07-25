a6dc181310944a5579830152d379370c501f1ede
Ali Najafi <mail.ali.najafi@gmail.com>
2015-07-25 11:42:38 +0430

    Port of my_nerd_bot for Google App Engine

---
+++ GoogleAppEngine/nerd_config.py
@@ -0,0 +1,18 @@
+#################################
+# NERD_BOT CONNFIGURE VARIABLES #
+#################################
+
+
+
+#Token
+TOKEN = 'TOKEN'
+
+
+# Save Image Path
+find_img_path = './images'
+
+
+
+# MongoDB
+mongo_db      = None
+mongo_version = None
